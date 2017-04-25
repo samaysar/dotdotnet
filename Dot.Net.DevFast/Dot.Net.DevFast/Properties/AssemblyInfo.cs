6bae5f7c478ea2ee6ab743edfca13f63127a379e
samaysar <samaysar@gmail.com>
2017-04-25 23:14:23 +0200

    #1
    AssemblyInfo file updated for TC patch.

--- Dot.Net.DevFast/Dot.Net.DevFast/Properties/AssemblyInfo.cs
+++ Dot.Net.DevFast/Dot.Net.DevFast/Properties/AssemblyInfo.cs
@@ -5,13 +5,17 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dot.Net.DevFast")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Contains extension methods for day to day usage")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Divya Sarthi MAHESHWARI")]
 [assembly: AssemblyProduct("Dot.Net.DevFast")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Divya Sarthi MAHESHWARI 2017")]
+[assembly: AssemblyTrademark("Divya Sarthi MAHESHWARI")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -33,3 +37,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
