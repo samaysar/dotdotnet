e699816d390cbbdeed1bb66b2db82422d272204f
samaysar <samaysar@gmail.com>
2017-04-25 23:44:53 +0200

    #1
    missed committed lines.

--- Dot.Net.DevFast/Dot.Net.DevFast/Properties/AssemblyInfo.cs
+++ Dot.Net.DevFast/Dot.Net.DevFast/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dot.Net.DevFast")]
+[assembly: AssemblyProduct("Dot.Net.DevFast")]
 [assembly: AssemblyDescription("Contains extension methods for day to day usage")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
@@ -12,7 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 [assembly: AssemblyCompany("Divya Sarthi MAHESHWARI")]
-[assembly: AssemblyProduct("Dot.Net.DevFast")]
 [assembly: AssemblyCopyright("Copyright © Divya Sarthi MAHESHWARI 2017")]
 [assembly: AssemblyTrademark("Divya Sarthi MAHESHWARI")]
 [assembly: AssemblyCulture("en-US")]
