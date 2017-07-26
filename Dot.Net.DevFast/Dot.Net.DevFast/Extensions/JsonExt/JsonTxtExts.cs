4644e94f443b745a5df91f2ebc8ce5e05a3aa8bd
samaysar <samaysar@gmail.com>
2017-07-26 14:47:38 +0200

    #3
    Added string builder overload for ToJsonAsync

--- Dot.Net.DevFast/Dot.Net.DevFast/Extensions/JsonExt/JsonTxtExts.cs
+++ Dot.Net.DevFast/Dot.Net.DevFast/Extensions/JsonExt/JsonTxtExts.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Dot.Net.DevFast.Etc;
 using Newtonsoft.Json;
 
 namespace Dot.Net.DevFast.Extensions.JsonExt
@@ -22,7 +26,7 @@ namespace Dot.Net.DevFast.Extensions.JsonExt
         /// <param name="obj">input object array to JSON serialize</param>
         /// <param name="serializer">JSON serializer to use</param>
         /// <param name="textWriter">target text writer</param>
-        /// <param name="disposeWriter">If true, textwriter is disposed after the serialization</param>
+        /// <param name="disposeWriter">If true, <paramref name="textWriter"/> is disposed after the serialization</param>
         public static Task ToJsonArrayAsync<T>(IEnumerable<T> obj, JsonSerializer serializer,
             TextWriter textWriter, bool disposeWriter = true)
         {
@@ -38,7 +42,7 @@ namespace Dot.Net.DevFast.Extensions.JsonExt
         /// <param name="serializer">JSON serializer to use</param>
         /// <param name="textWriter">target text writer</param>
         /// <param name="token">cancellation token to observe</param>
-        /// <param name="disposeWriter">If true, textwriter is disposed after the serialization</param>
+        /// <param name="disposeWriter">If true, <paramref name="textWriter"/> is disposed after the serialization</param>
         public static async Task ToJsonArrayAsync<T>(this IEnumerable<T> collection, JsonSerializer serializer,
             TextWriter textWriter, CancellationToken token, bool disposeWriter = true)
         {
@@ -91,6 +95,93 @@ namespace Dot.Net.DevFast.Extensions.JsonExt
 
         /// <summary>
         /// Writes the JSON serialized string of <paramref name="obj"/> using <paramref name="serializer"/>
+        /// to <paramref name="output"/> asynchronously.
+        /// <para>In case of <typeparamref name="T"/> as Large Collection, use 
+        /// <see cref="ToJsonArrayAsync{T}(IEnumerable{T},JsonSerializer,TextWriter,bool)"/> (or its variants), instead.</para>
+        /// </summary>
+        /// <typeparam name="T">Type of the input object to serialize</typeparam>
+        /// <param name="obj">input object to JSON serialize</param>
+        /// <param name="serializer">JSON serializer to use</param>
+        /// <param name="output">target output string builder</param>
+        /// <param name="formatProvider">Format provider. If null, then <seealso cref="CultureInfo.CurrentCulture"/> is used</param>
+        public static Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
+            StringBuilder output, IFormatProvider formatProvider = null)
+        {
+            return obj.ToJsonAsync(serializer, output, CancellationToken.None, formatProvider);
+        }
+
+        /// <summary>
+        /// Writes the JSON serialized string of <paramref name="obj"/> using <paramref name="serializer"/>
+        /// to <paramref name="output"/> asynchronously while observing <paramref name="token"/>.
+        /// <para>In case of <typeparamref name="T"/> as Large Collection, use 
+        /// <see cref="ToJsonArrayAsync{T}(IEnumerable{T},JsonSerializer,TextWriter,bool)"/> (or its variants), instead.</para>
+        /// </summary>
+        /// <typeparam name="T">Type of the input object to serialize</typeparam>
+        /// <param name="obj">input object to JSON serialize</param>
+        /// <param name="serializer">JSON serializer to use</param>
+        /// <param name="output">target output string builder</param>
+        /// <param name="token">cancellation token to observe</param>
+        /// <param name="formatProvider">Format provider. If null, then <seealso cref="CultureInfo.CurrentCulture"/> is used</param>
+        public static async Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
+            StringBuilder output, CancellationToken token, IFormatProvider formatProvider = null)
+        {
+            using (var textWriter = new StringWriter(output, formatProvider ?? CultureInfo.CurrentCulture))
+            {
+                await obj.ToJsonAsync(serializer, textWriter, token, false).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// Writes the JSON serialized string of <paramref name="obj"/> using <paramref name="serializer"/>
+        /// to <paramref name="outputStream"/> asynchronously.
+        /// <para>In case of <typeparamref name="T"/> as Large Collection, use 
+        /// <see cref="ToJsonArrayAsync{T}(IEnumerable{T},JsonSerializer,TextWriter,bool)"/> (or its variants), instead.</para>
+        /// </summary>
+        /// <typeparam name="T">Type of the input object to serialize</typeparam>
+        /// <param name="obj">input object to JSON serialize</param>
+        /// <param name="serializer">JSON serializer to use</param>
+        /// <param name="outputStream">target output stream</param>
+        /// <param name="enc">Text encoding to use. If null, then <seealso cref="Encoding.UTF8"/> is used.</param>
+        /// <param name="bufferSize">Buffer size</param>
+        /// <param name="disposeStream">If true, <paramref name="outputStream"/> is disposed after the serialization</param>
+        public static Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
+            Stream outputStream, Encoding enc = null, int bufferSize = StdLookUps.DefaultBufferSize, 
+            bool disposeStream = true)
+        {
+            return obj.ToJsonAsync(serializer, outputStream, CancellationToken.None, enc, bufferSize, disposeStream);
+        }
+
+        /// <summary>
+        /// Writes the JSON serialized string of <paramref name="obj"/> using <paramref name="serializer"/>
+        /// to <paramref name="outputStream"/> asynchronously while observing <paramref name="token"/>.
+        /// <para>In case of <typeparamref name="T"/> as Large Collection, use 
+        /// <see cref="ToJsonArrayAsync{T}(IEnumerable{T},JsonSerializer,TextWriter,bool)"/> (or its variants), instead.</para>
+        /// </summary>
+        /// <typeparam name="T">Type of the input object to serialize</typeparam>
+        /// <param name="obj">input object to JSON serialize</param>
+        /// <param name="serializer">JSON serializer to use</param>
+        /// <param name="outputStream">target output stream</param>
+        /// <param name="token">cancellation token to observe</param>
+        /// <param name="enc">Text encoding to use. If null, then <seealso cref="Encoding.UTF8"/> is used.</param>
+        /// <param name="bufferSize">Buffer size</param>
+        /// <param name="disposeStream">If true, <paramref name="outputStream"/> is disposed after the serialization</param>
+        public static async Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
+            Stream outputStream, CancellationToken token, Encoding enc = null, 
+            int bufferSize = StdLookUps.DefaultBufferSize, bool disposeStream = true)
+        {
+            using (var textWriter = new StreamWriter(outputStream, enc ?? Encoding.UTF8,
+                bufferSize, !disposeStream)
+            {
+                AutoFlush = true
+            })
+            {
+                await obj.ToJsonAsync(serializer, textWriter, token, false).ConfigureAwait(false);
+                await outputStream.FlushAsync(token).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// Writes the JSON serialized string of <paramref name="obj"/> using <paramref name="serializer"/>
         /// to <paramref name="textWriter"/> asynchronously.
         /// <para>In case of <typeparamref name="T"/> as Large Collection, use 
         /// <see cref="ToJsonArrayAsync{T}(IEnumerable{T},JsonSerializer,TextWriter,bool)"/> (or its variants), instead.</para>
@@ -99,7 +190,7 @@ namespace Dot.Net.DevFast.Extensions.JsonExt
         /// <param name="obj">input object to JSON serialize</param>
         /// <param name="serializer">JSON serializer to use</param>
         /// <param name="textWriter">target text writer</param>
-        /// <param name="disposeWriter">If true, textwriter is disposed after the serialization</param>
+        /// <param name="disposeWriter">If true, <paramref name="textWriter"/> is disposed after the serialization</param>
         public static Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
             TextWriter textWriter, bool disposeWriter = true)
         {
@@ -117,7 +208,7 @@ namespace Dot.Net.DevFast.Extensions.JsonExt
         /// <param name="serializer">JSON serializer to use</param>
         /// <param name="textWriter">target text writer</param>
         /// <param name="token">cancellation token to observe</param>
-        /// <param name="disposeWriter">If true, textwriter is disposed after the serialization</param>
+        /// <param name="disposeWriter">If true, <paramref name="textWriter"/> is disposed after the serialization</param>
         public static async Task ToJsonAsync<T>(this T obj, JsonSerializer serializer,
             TextWriter textWriter, CancellationToken token, bool disposeWriter = true)
         {
