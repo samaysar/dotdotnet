cc00a1b5faff12006db1e10b6624dbada5faa2e7
samaysar <samaysar@gmail.com>
2017-08-15 14:31:31 +0200

    #8
    SbReader coverage tests.

--- Dot.Net.DevFast/Dot.Net.DevFast.Tests/Extensions/Internals/SbReaderTest.cs
+++ Dot.Net.DevFast/Dot.Net.DevFast.Tests/Extensions/Internals/SbReaderTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using System.Threading.Tasks;
 using Dot.Net.DevFast.Etc;
 using Dot.Net.DevFast.Extensions.Internals;
 using NUnit.Framework;
@@ -87,6 +88,64 @@ namespace Dot.Net.DevFast.Tests.Extensions.Internals
             }
             Assert.True(sb.ToString().StartsWith("Test"));
             Assert.True(sb.ToString().EndsWith("Test1"));
+        }
+
+        [Test]
+        public void ReadLineAsync_Works_As_Expected_Without_Altering_StringBuilder()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Test");
+            sb.Append("Test1");
+            using (var sbreader = new SbReader(sb))
+            {
+                Assert.True(sbreader.ReadLineAsync().Result.Equals("Test"));
+                Assert.True(sbreader.ReadLineAsync().Result.Equals("Test1"));
+                Assert.Null(sbreader.ReadLineAsync().Result);
+            }
+            Assert.True(sb.ToString().StartsWith("Test"));
+            Assert.True(sb.ToString().EndsWith("Test1"));
+        }
+
+        [Test]
+        public void ReadToEndAsync_Works_As_Expected_Without_Altering_StringBuilder()
+        {
+            var sb = new StringBuilder("T");
+            using (var sbreader = new SbReader(sb))
+            {
+                Assert.True(sbreader.ReadToEndAsync().Result.Equals("T"));
+                Assert.True(sbreader.ReadToEndAsync().Result.Equals(""));
+            }
+            Assert.True(sb.ToString().Equals("T"));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void Buffered_ReadAsync_Works_As_Expected_Without_Altering_StringBuilder(int buffSize)
+        {
+            var sb = new StringBuilder("Ttt");
+            var chars = new char[buffSize];
+            using (var sbreader = new SbReader(sb))
+            {
+                Assert.True(sbreader.ReadAsync(chars, 0, buffSize).Result.Equals(Math.Min(3, buffSize)));
+                Assert.True(chars[0].Equals('T'));
+            }
+            Assert.True(sb.ToString().Equals("Ttt"));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(10)]
+        public void ReadBlockAsync_Works_As_Expected_Without_Altering_StringBuilder(int buffSize)
+        {
+            var sb = new StringBuilder("Ttt");
+            var chars = new char[buffSize];
+            using (var sbreader = new SbReader(sb))
+            {
+                Assert.True(sbreader.ReadBlockAsync(chars, 0, buffSize).Result.Equals(Math.Min(3, buffSize)));
+                Assert.True(chars[0].Equals('T'));
+            }
+            Assert.True(sb.ToString().Equals("Ttt"));
         }
     }
 }
