e4b7bd21495cdf159fc9021772f2209fc4d730a5
samaysar <samaysar@gmail.com>
2018-07-29 15:01:37 +0200

    #15 refactored everything as extensions on pipeline. pipeline is mutatable on each call.

---
+++ Dot.Net.DevFast/src/Dot.Net.DevFast/Extensions/StreamPipeExt/StreamPipe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dot.Net.DevFast.Extensions.StreamPipeExt
+{
+    /// <summary>
+    /// Elementary class that would support the whole functional streaming pipeline.
+    /// </summary>
+    public sealed class StreamPipe
+    {
+        private Func<Stream, bool, CancellationToken, Task> _writerFunc;
+
+        internal StreamPipe(Action<Stream, bool, CancellationToken> writerAction) : this(writerAction.ToAsync())
+        {
+        }
+
+        internal StreamPipe(Func<Stream, bool, CancellationToken, Task> writerFunc)
+        {
+            _writerFunc = writerFunc;
+        }
+
+        /// <summary>
+        /// Call to this method shall bootstrap the streaming pipeline and returns the associated asynchronous task that
+        /// starts writing on the <paramref name="stream"/>.
+        /// </summary>
+        /// <param name="stream">underlying stream</param>
+        /// <param name="dispose">If true, stream is dispose once operation completes.</param>
+        /// <param name="token">Cancellation token to observe</param>
+        public async Task StreamAsync(Stream stream, bool dispose = true,
+            CancellationToken token = default(CancellationToken))
+        {
+            await _writerFunc(stream, dispose, token).ConfigureAwait(false);
+        }
+
+        internal StreamPipe Mutate(Func<Func<Stream, bool, CancellationToken, Task>,
+            Func<Stream, bool, CancellationToken, Task>> mutable)
+        {
+            _writerFunc = mutable(_writerFunc);
+            return this;
+        }
+    }
+}
--- Dot.Net.DevFast/src/Dot.Net.DevFast/Extensions/StreamPipeExt/StreamPipeExts.cs
+++ Dot.Net.DevFast/src/Dot.Net.DevFast/Extensions/StreamPipeExt/StreamPipeExts.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Dot.Net.DevFast.Etc;
+using Dot.Net.DevFast.Extensions.JsonExt;
+using Dot.Net.DevFast.Extensions.StringExt;
 using Newtonsoft.Json;
 
 namespace Dot.Net.DevFast.Extensions.StreamPipeExt
@@ -19,13 +24,16 @@ namespace Dot.Net.DevFast.Extensions.StreamPipeExt
         /// <typeparam name="T">Type of object to serialize</typeparam>
         /// <param name="obj">Object to serialize as json text</param>
         /// <param name="serializer">if not provided, JsonSerializer with default values will be used.</param>
-        /// <param name="enc">Encoding to use while writing the file. If not supplied, by default <seealso cref="Encoding.UTF8"/>
+        /// <param name="enc">Encoding to use while writing the file. 
+        /// If not supplied, by default <seealso cref="Encoding.UTF8"/>
         /// (WwithOUT the utf-8 identifier, i.e. new UTF8Encoding(false)) will be used</param>
         /// <param name="writerBuffer">Buffer size for the stream writer</param>
-        public static IJsonPipe SerializeAsJson<T>(this T obj, JsonSerializer serializer = null,
-            Encoding enc = null, int writerBuffer = StdLookUps.DefaultFileBufferSize)
+        public static StreamPipe SerializeAsJson<T>(this T obj, 
+            JsonSerializer serializer = null,
+            Encoding enc = null, 
+            int writerBuffer = StdLookUps.DefaultFileBufferSize)
         {
-            return new JsonObjectPipe<T>(obj, serializer, enc ?? new UTF8Encoding(false), writerBuffer);
+            return new StreamPipe((s, d, t) => obj.ToJson(s, serializer, enc, writerBuffer, d));
         }
 
         /// <summary>
@@ -34,13 +42,16 @@ namespace Dot.Net.DevFast.Extensions.StreamPipeExt
         /// <typeparam name="T">Type of object to serialize</typeparam>
         /// <param name="obj">Object to serialize as json text</param>
         /// <param name="serializer">if not provided, JsonSerializer with default values will be used.</param>
-        /// <param name="enc">Encoding to use while writing the file. If not supplied, by default <seealso cref="Encoding.UTF8"/>
+        /// <param name="enc">Encoding to use while writing the file.
+        /// If not supplied, by default <seealso cref="Encoding.UTF8"/>
         /// (WwithOUT the utf-8 identifier, i.e. new UTF8Encoding(false)) will be used</param>
         /// <param name="writerBuffer">Buffer size for the stream writer</param>
-        public static IJsonPipe SerializeAsJson<T>(this IEnumerable<T> obj, JsonSerializer serializer = null,
-            Encoding enc = null, int writerBuffer = StdLookUps.DefaultFileBufferSize)
+        public static StreamPipe SerializeAsJson<T>(this IEnumerable<T> obj, 
+            JsonSerializer serializer = null,
+            Encoding enc = null, 
+            int writerBuffer = StdLookUps.DefaultFileBufferSize)
         {
-            return new JsonEnumeratorPipe<T>(obj, serializer, enc ?? new UTF8Encoding(false), writerBuffer);
+            return new StreamPipe((s, d, t) => obj.ToJsonArray(s, serializer, t, enc, writerBuffer, d));
         }
 
         /// <summary>
@@ -49,28 +60,105 @@ namespace Dot.Net.DevFast.Extensions.StreamPipeExt
         /// <typeparam name="T">Type of object to serialize</typeparam>
         /// <param name="obj">Object to serialize as json text</param>
         /// <param name="serializer">if not provided, JsonSerializer with default values will be used.</param>
-        /// <param name="enc">Encoding to use while writing the file. If not supplied, by default <seealso cref="Encoding.UTF8"/>
+        /// <param name="enc">Encoding to use while writing the file. 
+        /// If not supplied, by default <seealso cref="Encoding.UTF8"/>
         /// (WwithOUT the utf-8 identifier, i.e. new UTF8Encoding(false)) will be used</param>
         /// <param name="writerBuffer">Buffer size for the stream writer</param>
         /// <param name="pcts">source to cancel in case some error is encountered. Normally,
         /// this source token is observed at data producer side.</param>
-        public static IJsonPipe SerializeAsJson<T>(this BlockingCollection<T> obj, JsonSerializer serializer = null,
-            Encoding enc = null, int writerBuffer = StdLookUps.DefaultFileBufferSize,
+        public static StreamPipe SerializeAsJson<T>(this BlockingCollection<T> obj, 
+            JsonSerializer serializer = null,
+            Encoding enc = null, 
+            int writerBuffer = StdLookUps.DefaultFileBufferSize,
             CancellationTokenSource pcts = default (CancellationTokenSource))
         {
-            return new JsonBcPipe<T>(obj, serializer, pcts, enc ?? new UTF8Encoding(false), writerBuffer);
+            return new StreamPipe((s, d, t) => obj.ToJsonArrayParallely(s, serializer, t, pcts, enc, writerBuffer, d));
         }
 
         /// <summary>
         /// Compresses the data of given Stream pipe as source.
         /// </summary>
-        /// <param name="src">Source whose data to compress</param>
-        /// <param name="gzip">If true, <seealso cref="GZipStream"/> is used else <seealso cref="DeflateStream"/> is used</param>
+        /// <param name="src">Current pipe of the pipeline</param>
+        /// <param name="gzip">If true, <seealso cref="GZipStream"/> is used else 
+        /// <seealso cref="DeflateStream"/> is used</param>
         /// <param name="level">Compression level to use.</param>
-        public static ICompressedPipe ThenCompress(this IStreamPipe src, bool gzip = true,
+        public static StreamPipe ThenCompress(this StreamPipe src, 
+            bool gzip = true,
             CompressionLevel level = CompressionLevel.Optimal)
         {
-            return new CompressedPipe(src, gzip, level);
+            return src.Mutate(pipe => pipe.AddCompression(true, level));
+        }
+
+        /// <summary>
+        /// Call to this method shall bootstrap the streaming pipeline and returns the associated asynchronous task that 
+        /// starts saving stream data to file.
+        /// <para>An appropriate extension will be added to <paramref name="filename" /></para>
+        /// <para>Returns the <seealso cref="FileInfo"/> object of that the written file</para>
+        /// </summary>
+        /// <param name="src">Current pipe of the pipeline</param>
+        /// <param name="folder">folder path where file is saved</param>
+        /// <param name="filename">name of file. If not supplied a new GUID string will be used instead
+        /// <para>filename should NOT contain extension</para> </param>
+        /// <param name="fileStreamBuffer">Buffer size of the file stream</param>
+        /// <param name="options">File options</param>
+        /// <param name="token">Cancellation token to observe</param>
+        public static async Task<FileInfo> SaveAsFileAsync(this StreamPipe src, 
+            string folder,
+            string filename = null,
+            int fileStreamBuffer = StdLookUps.DefaultFileBufferSize,
+            FileOptions options = FileOptions.Asynchronous, 
+            CancellationToken token = default(CancellationToken))
+        {
+            return await src.SaveAsFileAsync(folder.ToDirectoryInfo(), filename, fileStreamBuffer, options, token)
+                .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Call to this method shall bootstrap the streaming pipeline and returns the associated asynchronous task that 
+        /// starts saving stream data to file.
+        /// <para>An appropriate extension will be added to <paramref name="filename" /></para>
+        /// <para>Returns the <seealso cref="FileInfo"/> object of that the written file</para>
+        /// </summary> 
+        /// <param name="src">Current pipe of the pipeline</param>
+        /// <param name="folder">directory information of folder where file will be created</param>
+        /// <param name="filename">name of file. If not supplied a new GUID string will be used instead
+        /// <para>filename should NOT contain extension</para> </param>
+        /// <param name="fileStreamBuffer">Buffer size of the file stream</param>
+        /// <param name="options">File options</param>
+        /// <param name="token">Cancellation token to observe</param>
+        public static async Task<FileInfo> SaveAsFileAsync(this StreamPipe src, 
+            DirectoryInfo folder, 
+            string filename = null,
+            int fileStreamBuffer = StdLookUps.DefaultFileBufferSize, 
+            FileOptions options = FileOptions.Asynchronous,
+            CancellationToken token = default(CancellationToken))
+        {
+            var targetFile = folder.CreateFileInfo(filename ?? Guid.NewGuid().ToString("N"));
+            await src.SaveAsFileAsync(targetFile, fileStreamBuffer, options, token).ConfigureAwait(false);
+            return targetFile;
+        }
+
+        /// <summary>
+        /// Call to this method shall bootstrap the streaming pipeline and returns the associated asynchronous task that 
+        /// starts saving stream data to file.
+        /// </summary>
+        /// <param name="src">Current pipe of the pipeline</param>
+        /// <param name="fileinfo">file info object of the file to create/rewrite.</param>
+        /// <param name="fileStreamBuffer">Buffer size of the file stream</param>
+        /// <param name="options">File options</param>
+        /// <param name="token">Cancellation token to observe</param>
+        public static async Task SaveAsFileAsync(this StreamPipe src, 
+            FileInfo fileinfo, 
+            int fileStreamBuffer = StdLookUps.DefaultFileBufferSize,
+            FileOptions options = FileOptions.Asynchronous, 
+            CancellationToken token = default(CancellationToken))
+        {
+            using (var strm = fileinfo.CreateStream(FileMode.Create, FileAccess.ReadWrite, FileShare.Read,
+                fileStreamBuffer, options))
+            {
+                await src.StreamAsync(strm, false, token).ConfigureAwait(false);
+                await strm.FlushAsync(token).ConfigureAwait(false);
+            }
         }
     }
 }
